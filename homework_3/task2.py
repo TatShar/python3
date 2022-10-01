49d561a85142fe12960aa704bcfea9d953e8009a
TatShar <tvku@yandex.ru>
2022-10-01 13:44:06 +0300

    task 1,2,3

---
+++ homework_3/task2.py
@@ -0,0 +1,21 @@
+#Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+lst = [2,3,5,6]
+lst1=[]
+y=0
+z=len(lst)-1
+for i in range (len(lst)):
+    if len(lst)%2!=0:
+        if i<= round (len(lst)/2):
+            lst1.append(lst[y]*lst[z])
+            y+=1
+            z-=1
+        else: break
+    else:
+         if i< round (len(lst)/2):
+            lst1.append(lst[y]*lst[z])
+            y+=1
+            z-=1
+
+print(lst1)
+
