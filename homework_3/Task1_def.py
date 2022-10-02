24fe8aea17e07b579375f69dd14bdcee5c80c92d
TatShar <tvku@yandex.ru>
2022-10-02 08:31:50 +0300

    Task1_def

--- homework_3/Task1_def.py
+++ homework_3/Task1_def.py
@@ -1,9 +1,26 @@
 #Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
-def sum ():
-    lst =[1,2,3,4,5,6,]
+import random
+
+def lst():
+    number = int (input('input a number '))
+    ls=[]
+    for i in range(number):
+        ls.append (random.randint(1,10))
+    return ls
+#n=int(input('Enter a number '))
+#ls=[]
+#for i in range (n):
+    #ls.append(random.randint(1,10))
+#print(ls)
+ls = lst()
+print (ls)
+
+def sum (lst):
     sum = 0
     for i in range (len(lst)):
-        if i%2!=0:
-            sum+=lst[i]
+        if i%2==0:
+           sum+=lst[i]
     return sum
-print (sum ())
+
+
+print (sum (ls))
