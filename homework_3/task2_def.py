316f967f210c77fc9026a4d2ced21037abde6042
TatShar <tvku@yandex.ru>
2022-10-02 21:50:17 +0300

    Task2_def

--- homework_3/task2_def.py
+++ homework_3/task2_def.py
@@ -1,22 +1,22 @@
 #Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 #Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-def mult():
-    lst = [2,3,5,6]
+
+from random import sample
+
+def fill_list(number):
+    ls=sample(range(1,number*2),number)
+    return ls
+
+def multy(lst):
     lst1=[]
-    y=0
-    z=len(lst)-1
-    for i in range (len(lst)):
-        if len(lst)%2!=0:
-            if i<= round (len(lst)/2):
-                lst1.append(lst[y]*lst[z])
-                y+=1
-                z-=1
-            else: break
-        else:
-            if i< round (len(lst)/2):
-                lst1.append(lst[y]*lst[z])
-                y+=1
-                z-=1
+    len_lst=len(lst)//2
+    for i in range(len_lst):
+        lst1.append(lst[i]*lst[len(lst)-1-i])
+    if len(lst)%2:
+        lst1.append(lst[len(lst)//2])
     return lst1
-print(mult())
+
+start_list=fill_list(int(input('Enter a number ')))
+print (start_list)
+print (multy(start_list))
